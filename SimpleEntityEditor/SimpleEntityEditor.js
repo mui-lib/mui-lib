9c9f5394be22fd3ee245f8a62d9b7dc69c610691
Yu Zhanbei <awesome.zhanbei@gmail.com>
2018-10-28 21:03:49 +0800

    [SimpleEntityEditor] Generate _session_id on #targetEntity changed.

--- SimpleEntityEditor/SimpleEntityEditor.js
+++ SimpleEntityEditor/SimpleEntityEditor.js
@@ -3,11 +3,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Pass a different _session_id to reset states of the #AdvancedTextField which holds the state of the #edited status.
+// Different sessions will be used for different target entities.
+let _session_id = 1;
+
 // A custom entity editor/form consisting of configurable components for entity fields, which calls the
 // props#onPatchChange() with the latest #entityPatch when any field is going to be updated.
 //
 // Think about whether to extend #PureComponent or not, and currently use #PureComponent(performance, logically correctness).
 class SimpleEntityEditor extends React.PureComponent {
+	static getDerivedStateFromProps(nextProps, prevState) {
+		const {_target_entity} = prevState;
+		const {targetEntity} = nextProps;
+		if (_target_entity !== targetEntity) {
+			// Generate a new session ID for the new #targetEntity.
+			return {_session_id: _session_id++, _target_entity: targetEntity};
+		}
+		return null;
+	}
+
+	state = {
+		_target_entity: null,
+	};
+
 	onValueChange = ({target: {id, name, value}} = {}) => {
 		if (!id) {console.warn(`The id of target is empty, id: [${id}]; name: [${name}].`);}
 		if (!id) {id = name;}
@@ -21,7 +39,7 @@ class SimpleEntityEditor extends React.PureComponent {
 		onPatchChange(entityPatch);
 	};
 
-	renderField = (field, value, env) => {
+	renderField = (field, value, env, _session_id) => {
 		let {getLabel, getPlaceholder, getHelperText, getErrorText, default: _default, ...props} = field;
 		const id = props.id;
 		// The label, placeholder, and helperText are usually not dynamic.
@@ -49,6 +67,8 @@ class SimpleEntityEditor extends React.PureComponent {
 		// break;
 		// }
 		const UsedFieldEditor = FieldEditor || this.props.TextField;
+		// Pass the _session_id if the used component desires.
+		if (UsedFieldEditor.propTypes._session_id) {props._session_id = _session_id;}
 		// FIX-ME Currently delete props.type to fix the issue where the height of multi-lined #Text-Field is not flexible.
 		if (props.type && props.multiline) {delete props.type;}
 		return (
@@ -63,6 +83,7 @@ class SimpleEntityEditor extends React.PureComponent {
 
 	render() {
 		const {entityFields, targetEntity = {}, entityPatch = {}} = this.props;
+		const {_session_id} = this.state;
 		const updatedEntity = entityPatch ? {...targetEntity, ...entityPatch} : {...targetEntity};
 		const isCreating = Boolean(targetEntity._id) || Boolean(targetEntity.id);
 		const env = {
@@ -70,7 +91,7 @@ class SimpleEntityEditor extends React.PureComponent {
 			// The updated entity.
 			entity: updatedEntity,
 		};
-		return entityFields.map(field => this.renderField(field, updatedEntity[field.id], env));
+		return entityFields.map(field => this.renderField(field, updatedEntity[field.id], env, _session_id));
 	}
 }
 
