17624bcc561f32aa28a08147f5897f92cd33153c
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-07-13 20:47:28 +0800

    [CountdownBySeconds] Re-implemented component with #useState/#useEffect.
    - A better idea may be implemented again.

--- CountdownBySeconds/CountdownBySeconds.tsx
+++ CountdownBySeconds/CountdownBySeconds.tsx
@@ -1,48 +1,39 @@
 'use strict';
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 
 // Use 999 milliseconds as 1 second for rendering and other operations may slow it down.
 const timeout = 999;
 
-// A simple #Countdown by seconds.
-class CountdownBySeconds extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			seconds: props.seconds || 5,
-		};
-	}
-
-	componentWillMount() {
-		this.mTimer = setTimeout(this.doCountDown, timeout);
-	}
-
-	componentWillUnmount() {
-		if (this.mTimer) {clearTimeout(this.mTimer);}
-	}
-
-	doCountDown = () => {
-		const {seconds} = this.state;
-		if (this.mTimer) {clearTimeout(this.mTimer);}
-		if (seconds <= 0) {return;}
-		this.setState({
-			seconds: seconds - 1,
-		});
-		this.mTimer = setTimeout(this.doCountDown, timeout);
-	};
-
-	render() {
-		const {seconds} = this.state;
-		return (
-			<span>{seconds}</span>
-		);
-	}
+interface IProps {
+	seconds: number;
 }
 
-CountdownBySeconds.propTypes = {
-	seconds: PropTypes.number.isRequired,
+// A simple #Countdown by seconds.
+const TheCountdownBySeconds = (props: IProps) => {
+	const [seconds, setState] = useState(props.seconds || 5);
+	// console.log('seconds:', seconds);
+
+	// Executed on seconds changed.
+	// Register a new timer and prepare the cleanup.
+	// OR Think about follow the rule to clean the latest timer if any.
+	useEffect(() => {
+		const mTimer = setTimeout(doCountDown, timeout);
+		// console.log('mTimer set >:', mTimer);
+		return () => {
+			// console.log('cleaning up >', mTimer);
+			if (mTimer) {clearTimeout(mTimer);}
+		};
+	}, [seconds]);
+
+	const doCountDown = () => {
+		if (seconds <= 0) {return;}
+		setState(seconds - 1);
+	};
+
+	return (
+		<span>{seconds}</span>
+	);
 };
 
-export default CountdownBySeconds;
+export const CountdownBySeconds: React.FC<IProps> = React.memo<IProps>(TheCountdownBySeconds);
