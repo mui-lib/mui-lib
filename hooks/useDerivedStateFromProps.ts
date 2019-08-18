7e53bc93e77541449b6f44ba2ccb788fd0e71f42
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-08-18 13:37:00 +0800

    [hooks/useDerivedStateFromProps] Supported state #Auto-merging like Class.

--- hooks/useDerivedStateFromProps.ts
+++ hooks/useDerivedStateFromProps.ts
@@ -3,7 +3,8 @@
 import React from 'react';
 
 type StateSetterAction<IState> = (state: IState) => IState
-type SetStateAction<IState> = IState | StateSetterAction<IState>
+// @see https://stackoverflow.com/questions/39713349/make-all-properties-within-a-typescript-interface-optional
+type SetStateAction<IState> = Partial<IState> | StateSetterAction<IState>
 
 // Initialize state and reset state with specific conditions.
 // MAY BE USED TO Get the derived state from the context(props/states) depends on context(props/states).
@@ -19,7 +20,9 @@ export const useDerivedStateFromProps = <IState>(getDerivedState: () => IState, 
 	const doForceRender = React.useState<any>()[1];
 	const setState = (state: SetStateAction<IState>) => {
 		if (typeof state === 'object') {
-			ref.current = state;
+			// FIX-ME Auto merge the state like the *Class Components* do.
+			// @see https://reactjs.org/docs/hooks-reference.html#functional-updates
+			ref.current = {...ref.current, ...state};
 		} else if (typeof state === 'function') {
 			const setStateAction: StateSetterAction<IState> = state as StateSetterAction<IState>;
 			ref.current = setStateAction(ref.current);
