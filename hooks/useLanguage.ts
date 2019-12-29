581c69fa65191862fade629dd525b0f705fa817f
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-29 22:36:22 +0800

    [hooks/useLanguage|resources/languages] Added context hook of languages.

---
+++ hooks/useLanguage.ts
@@ -0,0 +1,25 @@
+//
+
+import React from 'react';
+import {ILanguagesBundle, UN_LANGUAGES} from '../resources/languages';
+
+interface IContextLanguageValue {
+	language: string;
+	languages: readonly string[];
+}
+
+export const DEFAULT_LANGUAGE_CONTEXT_VALUE: IContextLanguageValue = {
+	language: navigator.language || UN_LANGUAGES.ENGLISH,
+	languages: navigator.languages,
+};
+
+// @see https://reactjs.org/docs/hooks-reference.html#usecontext
+const ContextLanguage = React.createContext(DEFAULT_LANGUAGE_CONTEXT_VALUE);
+export const ContextLanguageProvider = ContextLanguage.Provider;
+
+// Call hooks in functional components.
+export const useLanguageContext = (): IContextLanguageValue => React.useContext(ContextLanguage);
+// Get the preferred language from context.
+export const usePreferredLanguageFromContext = (): string => React.useContext(ContextLanguage).language;
+// Get the localized resources( of strings) by the preferred language from context.
+export const useLocalizedResourcesFromContext = <T>(bundle: ILanguagesBundle<T>): T => bundle[usePreferredLanguageFromContext()] || bundle.df;
---
+++ resources/languages.ts
@@ -0,0 +1,28 @@
+//
+
+// The code of UN languages.
+// @see https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+// @see https://www.un.org/en/sections/about-un/official-languages/
+// Arabic, Chinese, English, French, Russian and Spanish
+export const UN_LANGUAGES = {
+	ARABIC: 'ar',
+	CHINESE: 'zh',
+	ENGLISH: 'en',
+	FRENCH: 'fr',
+	RUSSIAN: 'ru',
+	SPANISH: 'es',
+};
+
+export interface IUnLanguages<T = string> {
+	ar?: T;
+	zh?: T;
+	en?: T;
+	fr?: T;
+	ru?: T;
+	es?: T;
+}
+
+// The bundle of default language and un languages.
+export interface ILanguagesBundle<T> extends IUnLanguages<T> {
+	df: T;
+}
