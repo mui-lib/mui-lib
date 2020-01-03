a7ff0357bdb206363c4f80d5bd5afe392afbfb35
Yu Zhanbei <awesome.zhanbei+shuxi@gmail.com>
2020-01-03 17:48:05 +0800

    [hooks/usePromiseFetcher] Update no state of unmounted components.

--- hooks/usePromiseFetcher.ts
+++ hooks/usePromiseFetcher.ts
@@ -4,23 +4,27 @@ import React from 'react';
 import {IFetcher, useFetcher} from './useFetcher';
 
 // By default, will trigger the async promise directly; provide a #doRefresh API to refresh data.
-// FIX-ME No update/setState when the component unmount.
 export const usePromiseFetcher = <T, E = Error>(doFetch: () => Promise<T>, notFetching?: boolean): [IFetcher<T, E>, (noBlinking?: boolean) => any] => {
+	const isAlive = React.useRef(true);
 	const [fetcher, fetched, refreshing] = useFetcher<T, E>(notFetching);
 	const doRefresh = (noBlinking?: boolean) => {
 		if (!noBlinking) {refreshing();}
 		// console.log('refreshing/noBlinking', noBlinking);
 		doFetch().then(assets => {
-			fetched(assets);
+			// No update/setState call for unmount components.
+			isAlive.current && fetched(assets);
 			// console.log('fetched(assets);', assets);
+			return assets;
 		}).catch(ex => {
-			fetched(undefined, ex);
+			isAlive.current && fetched(undefined, ex);
 			// console.log('failed to fetch:', ex);
+			// Here, the exception hence is swallowed.
+			// return ex;
 		});
 	};
 	React.useEffect(() => {
-		if (notFetching) {return;}
-		doRefresh(true);
+		if (!notFetching) {doRefresh(true);}
+		return () => {isAlive.current = false;};
 	}, []);
 	return [fetcher, doRefresh];
 };
