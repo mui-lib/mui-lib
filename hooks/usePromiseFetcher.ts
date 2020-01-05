b8c954b2d72ca687e335283d9dddf4182e7bb9db
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-01-05 11:49:13 +0800

    [hooks/usePromiseFetcher] Supported #depends to reset fetcher on update.

--- hooks/usePromiseFetcher.ts
+++ hooks/usePromiseFetcher.ts
@@ -2,30 +2,40 @@
 
 import React from 'react';
 import {IFetcher, useFetcher} from './useFetcher';
-import {useRefer} from './useRefer';
 
 // By default, will trigger the async promise directly; provide a #doRefresh API to refresh data.
-export const usePromiseFetcher = <T, E = Error>(doFetch: () => Promise<T>, notFetching?: boolean): [IFetcher<T, E>, (noBlinking?: boolean) => any] => {
-	const [isAlive, setAlive] = useRefer(true);
+export const usePromiseFetcher = <T, E = Error>(doFetch: () => Promise<T>, depends: any[] = [], notFetching?: boolean): [IFetcher<T, E>, (noBlinking?: boolean) => any] => {
+	const ref = React.useRef(true);
 	const [fetcher, fetched, refreshing] = useFetcher<T, E>(notFetching);
+	// console.log('updating fetcher:', fetcher);
 	const doRefresh = (noBlinking?: boolean) => {
 		if (!noBlinking) {refreshing();}
 		// console.log('refreshing/noBlinking', noBlinking);
 		doFetch().then(assets => {
 			// No update/setState call for unmount components.
-			isAlive && fetched(assets);
+			ref.current && fetched(assets);
 			// console.log('fetched(assets);', assets);
 			return assets;
 		}).catch(ex => {
-			isAlive && fetched(undefined, ex);
+			ref.current && fetched(undefined, ex);
 			// console.log('failed to fetch:', ex);
 			// Here, the exception hence is swallowed.
 			// return ex;
 		});
 	};
 	React.useEffect(() => {
-		if (!notFetching) {doRefresh(true);}
-		return () => {setAlive(false);};
-	}, []);
+		// FIX-ME Cancel the request of same component with different dependencies.
+		if (ref.current) {
+			// Initialization of the Target Component
+			// Should not update state again, during the initialization process of the target component.
+			if (!notFetching) {doRefresh(true);}
+		} else {
+			// Reset of the Target Component, Requested by Dependencies
+			ref.current = true;
+			// Should reset the fetcher explicitly, because the current fetcher holds an incorrect cache.
+			doRefresh();
+		}
+		return () => {ref.current = false;};
+	}, depends);
 	return [fetcher, doRefresh];
 };
