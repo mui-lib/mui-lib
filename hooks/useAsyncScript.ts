58a47ae2dddf7c65b0b505452033a1249df74937
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-04-02 08:47:27 +0800

    [hooks/useAsyncScript] Added hook to load external script dynamically.

---
+++ hooks/useAsyncScript.ts
@@ -0,0 +1,21 @@
+//
+
+import React from 'react';
+import {loadScript} from 'src/utils/HtmlScriptLoader';
+
+export const useAsyncScript = (url: string, isLoaded: () => boolean): [boolean] => {
+	const [loaded, setLoaded] = React.useState(isLoaded);
+
+	React.useLayoutEffect(() => {
+		if (isLoaded()) {return;}
+		console.warn('Loading the depended script now!', new Date().toLocaleString());
+		loadScript(url).then(() => {
+			console.log('Loaded the target script:', url, new Date().toLocaleString());
+			setLoaded(true);
+		}).catch((ex) => {
+			console.error('Failed to load the target script:', ex, new Date().toLocaleString());
+		});
+	}, [loaded]);
+
+	return [loaded];
+};
